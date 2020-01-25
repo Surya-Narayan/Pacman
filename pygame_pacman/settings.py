91ad08c0675e4c0375325c41ffbb335078011faa
dgdheeraj <gharde.dheeraj@gmail.com>
2020-01-25 16:21:56 +0530

    Create settings.py

---
+++ pygame_pacman/settings.py
@@ -0,0 +1,15 @@
+#Screen Settings
+WIDTH , HEIGHT = 560,620
+FPS=60
+
+#color settings
+BLACK=(0,0,0)
+RED=(200,20,20)
+GREY=(107,107,107)
+
+#font settings
+START_TEXT_SIZE=16
+START_FONT='arial black'
+#player settings
+
+#mob settings
